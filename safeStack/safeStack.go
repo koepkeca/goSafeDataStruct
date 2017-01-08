09dc45352e5c91672fab74ddfa213dd648f8b726
root <koepkeca@gmail.com>
2017-01-08 06:15:26 -0600

    Initial Commit

---
+++ safeStack/safeStack.go
@@ -0,0 +1,78 @@
+//package safeStack implements a thread safe stack in go.
+package safeStack
+
+//SafeStack is the structure that contains the channel used to
+//communicate with the stack.
+type SafeStack struct {
+	op chan (func(*stack))
+}
+
+//Len will get return the number of items in the stack.
+func (s *SafeStack) Len() (i int64) {
+	lChan := make(chan int64)
+	s.op <- func(curr *stack) {
+		lChan <- curr.size
+	}
+	return <-lChan
+}
+
+//Pop will perform a pop on the stack, removing the first item
+//and returning it's value.
+func (s *SafeStack) Pop() (v interface{}) {
+	vChan := make(chan interface{})
+	s.op <- func(curr *stack) {
+		if curr.size == 0 {
+			vChan <- nil
+		}
+		val := curr.top.value
+		curr.top = curr.top.next
+		curr.size--
+		vChan <- val
+	}
+	return <-vChan
+}
+
+//Push will push the value v onto the stack.
+func (s *SafeStack) Push(v interface{}) {
+	s.op <- func(curr *stack) {
+		curr.top = &elem{v, curr.top}
+		curr.size++
+	}
+}
+
+//Destory closes the primary channel thus stopping
+//the running go-routine.
+func (s *SafeStack) Destroy() {
+	close(s.op)
+}
+
+//NewSafeStack creates a new Safe Stack, this also starts the go-routine
+//so once this is called, you need to clean up after yourself
+//by using the Destroy method.
+func NewSafeStack() (s *SafeStack) {
+	s = &SafeStack{make(chan func(*stack))}
+	go s.loop()
+	return
+}
+
+//stack is the basic container for the stack
+type stack struct {
+	top  *elem
+	size int64
+}
+
+//elem is the element structure
+type elem struct {
+	value interface{}
+	next  *elem
+}
+
+//loop creates the guarded data structure and listens for
+//methods on the op channel. loop terminates when the op
+//channel is closed.
+func (s *SafeStack) loop() {
+	st := &stack{}
+	for op := range s.op {
+		op(st)
+	}
+}
---
+++ safeStack/safeStack_test.go
@@ -0,0 +1,73 @@
+package safeStack
+
+import (
+	"testing"
+)
+
+func TestStackCreation(t *testing.T) {
+	s := NewSafeStack()
+	if s.Len() != 0 {
+		t.Errorf("Failed, invalid stack length.")
+	}
+	s.Destroy()
+	return
+}
+
+func TestStackPushLength(t *testing.T) {
+	s := NewSafeStack()
+	s.Push(14)
+	s.Push(42)
+	s.Push("testing")
+	s.Push([]byte("Viper"))
+	len := s.Len()
+	if len != 4 {
+		t.Errorf("Failed, invalid stack length, got %d expected 4", len)
+	}
+	s.Destroy()
+	return
+}
+
+func TestStackPushOrder(t *testing.T) {
+	var nv = 0
+	var ok = false
+	s := NewSafeStack()
+	s.Push(16)
+	s.Push(32)
+	s.Push(64)
+	if nv, ok = s.Pop().(int); !ok {
+		t.Errorf("Failed, Pop got wrong type")
+		s.Destroy()
+		return
+	}
+	if nv != 64 {
+		t.Errorf("Failed, got incorrect value order")
+		s.Destroy()
+		return
+	}
+	s.Destroy()
+	return
+}
+
+func TestSizeAfterPop(t *testing.T) {
+	s := NewSafeStack()
+	s.Push(16)
+	s.Push("test")
+	s.Push("私は笑い男だ")
+	_ = s.Pop()
+	_ = s.Pop()
+	_ = s.Pop()
+	if s.Len() != 0 {
+		t.Errorf("Failed, poped through entire stack, yet size is non-zero")
+	}
+	s.Destroy()
+	return
+}
+
+func TestEmptyPop(t *testing.T) {
+	s := NewSafeStack()
+	v := s.Pop()
+	if v != nil {
+		t.Errorf("Empty Pop got non-nil value")
+	}
+	s.Destroy()
+}
